594e60f23bf149500dedb01e21f73f9dc2ad80a2
chiqui1986 <45525387+chiqui1986@users.noreply.github.com>
2021-09-03 11:16:34 -0300

    refactor & doc

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -2,43 +2,49 @@
 #1 Agrupo x usuario
 #2 Ordeno cronologicamente SECUENCIAS
 #3 Arma y agrupa secuencias 
-#4 Obtiene frecuencias correspondientes
-#5 Ordena por frecuencia desc
+#4 Cuenta frecuencia
+#5 Ordena por frecuencia
 
-logFile = open("fakelog.txt", "r")
-unique_user_list, users_keys = [], []
+def find_top_ten(route: str) -> list:
 
-for line in logFile:    # Unpack de registros del log
-    user_id, time_stamp, page = line.strip().split(",")
-    users_visits = [user_id, time_stamp, page]
-    unique_user_list.append(users_visits)    
-    if user_id not in users_keys: #obtengo lista de usuarios unicos
-        users_keys.append(user_id)
+    f = open(route, "r")
+    unique_user_list, users_keys = [], []
 
-users_dict = {k: [] for k in users_keys} # Arma dict de usuarios y sus respectivas page visits y timestamps
+    for line in f:    # Unpack de registros
+        user_id, time_stamp, page = line.strip().split(",")
+        users_visits = [user_id, time_stamp, page]
+        unique_user_list.append(users_visits)    
+        if user_id not in users_keys: 
+            users_keys.append(user_id)
 
-# Se busca que secuencias de 3 visitas fueron realizadas ==> agrega visita para c/ usuario y reordena cronologica/timestamp 
-for user in unique_user_list:
-    user_id, time_stamp, page = user
-    users_dict[user_id].append((time_stamp, page))
-    users_dict[user_id] = sorted(users_dict[user_id])
+    users_dict = {k: [] for k in users_keys}
 
-triplets_frequencies, triplets_combinations, triplets_to_sort = [], [], []
+    # Ordena secuencias de visitas 
+    for user in unique_user_list:
+        user_id, time_stamp, page = user
+        users_dict[user_id].append((time_stamp, page))
+        users_dict[user_id] = sorted(users_dict[user_id])
 
-for userId in users_dict:    
-    # para los usuarios con 3 o mas visitas, arma secuencias/triplet y agrupa
-    if len(users_dict[userId]) > 2 :
-        for i in range(len(users_dict[userId])-2):
-            triplet = (users_dict[userId][i][1], users_dict[userId][i+1][1], users_dict[userId][i+2][1])
+    triplets_frequencies, triplets_combinations, triplets_to_sort = [], [], []
+
+    for user_page_visits in users_dict:    
+        # para los usuarios con 3 o mas visitas, arma secuencias/triplet y agrupa
+        if len(users_dict[user_page_visits]) > 2 :
+            for i in range(len(users_dict[user_page_visits])-2):
+                triplet = tuple(page_visit[1] for page_visit in users_dict[user_page_visits][i:i+3])
           
-            if triplet in triplets_combinations:
-                triplets_frequencies[triplets_combinations.index(triplet)] += 1
-            else:
-                triplets_frequencies.append(1)
-                triplets_combinations.append(triplet)
+                if triplet not in triplets_combinations:
+                    triplets_frequencies.append(1)
+                    triplets_combinations.append(triplet)
+                
+                else:
+                    triplets_frequencies[triplets_combinations.index(triplet)] += 1
 
-for t, f in zip(triplets_combinations, triplets_frequencies):
-    triplets_to_sort.append([f, t])
+    for triplets, frequency in zip(triplets_combinations, triplets_frequencies):
+        triplets_to_sort.append([triplets, frequency])
 
-top10 = sorted(triplets_to_sort, reverse = True)[:10]
-print(top10)
+    triplets_to_sort.sort(key=lambda x: x[1], reverse=True )
+    return (triplets_to_sort[:10])
+
+print("Triplets & frequency:")
+print(find_top_ten("fakelog.txt"))
